1942f76aa3dafef23491ebf58dabd4ff9f12245a
twodayup <55915003+twodayup@users.noreply.github.com>
2019-09-28 21:26:18 +0800

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,2 +1,2 @@
-var html = '<img src="https://www.xb588.net/newbbs/20190906_1527_425.jpg">';
-$('#ads').html(html);
+//var html = '<img src="https://www.xb588.net/newbbs/20190906_1527_425.jpg">';
+//$('#ads').html(html);
